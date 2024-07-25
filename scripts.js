8078aa764db5f1aced33b1393086d1b2fe5dfe6b
XanzsmeE <xanzsmeee@gmail.com>
2024-07-25 20:02:27 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('profileImg').addEventListener('click', function() {
+    this.classList.toggle('enlarged');
+});
+
+document.getElementById('waLink').addEventListener('click', function() {
+    window.open('https://wa.me/6281354718652?text=Halo%20Gamaliel', '_blank');
+});
